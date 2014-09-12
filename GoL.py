1494ecdab21e49868f9ff3d70e1a0dc38a3b63e1
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-12 22:11:55 +0000

    main loop nearly complete

--- GoL.py
+++ GoL.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+import os
+
 from config import HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE
 from app.cells import Cell
 
@@ -12,9 +15,26 @@ def let_there_be_life(hsize, vsize, birth_neighbors, life_neighbors):
 
 
 life = let_there_be_life(HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE)
-print life
+possible_neighbors = []
 
 
 while __name__ == '__main__':
-    break
+    for i in xrange(VSIZE):
+        for j in xrange(HSIZE):
+            cell = life[j][i]
+            possible_neighbors = []
+
+            possible_neighbors = cell.find_neighbors(HSIZE, VSIZE)
+
+            for neighbor in possible_neighbors:
+                cell.check_neighbor(life[neighbor[0]][neighbor[1]])
+
+            cell.reap_and_sew()
+
+            if cell.status == 0:
+                print(' ', end='')
+            else:
+                print('O', end='')
+        print('\n')
+    os.system('clear')
 
