0987265058be61336ea3c24786507bae92e10714
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-12 20:29:27 +0000

    comment out count_neighbors for now

--- app/cells.py
+++ app/cells.py
@@ -1,4 +1,5 @@
 class Cell(object):
+
     def __init__(self, xpos, ypos, numtobirth, numtolive):
         self.neighbors = 0
         self.numtobirth = numtobirth
@@ -6,18 +7,27 @@ class Cell(object):
         self.status = ''
         self.xpos = xpos
         self.ypos = ypos
-        self.pos = [self.xpos, self.ypos]
+        self.pos = (xpos, ypos)
 
+    def check_neighbors(self, xpos, ypos):
+        if life[xpos][ypos].satus == 1:
+            self.neighbors = self.neighbors + 1
+
+    '''
     def count_neighbors(self, live_board):
+        if self.ypos != 0:
+            
+            if self.xpos != 0:
+                '''
 
 
-    def create(self, neighbors):
-        if self.status == 'dead'
+    def be_born(self, neighbors):
+        if self.status == 0:
             if neighbors in self.numtobirth:
-                self.status = 'alive'
+                self.status = 1
 
-    def destroy(self, neighbors):
-        if self.status == 'alive':
+    def die(self, neighbors):
+        if self.status == 1: 
             if neighbors not in self.numtolive:
-                self.status = 'dead'
+                self.status = 0
 
