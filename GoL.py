57a549f48e8c67f26602e8ddf1b049412a5e5df7
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-12 21:15:05 +0000

    put life array in let_there_be_life()

--- GoL.py
+++ GoL.py
@@ -2,15 +2,18 @@ from config import HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE
 from app.cells import Cell
 
 
-life = [[None for j in xrange(VSIZE)] for i in xrange(HSIZE)]
 def let_there_be_life(hsize, vsize, birth_neighbors, life_neighbors):
+    life = [[None for j in xrange(vsize)] for i in xrange(hsize)]
     for i in xrange(vsize):
         for j in xrange(hsize):
             life[j][i] = (Cell(j, i, birth_neighbors, life_neighbors))
 
+    return life
 
-let_there_be_life(HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE)
+
+life = let_there_be_life(HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE)
 print life
+
 
 while __name__ == '__main__':
     break
