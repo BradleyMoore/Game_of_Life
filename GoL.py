ddcec04036e63d68a0eef024ec3efa5f88041b9f
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-12 20:28:25 +0000

    all cells are now contained in life list

--- GoL.py
+++ GoL.py
@@ -1,7 +1,17 @@
-from app.cells import Cell
 from config import HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE
+from app.cells import Cell
 
+
+life = [[None for j in xrange(VSIZE)] for i in xrange(HSIZE)]
+def let_there_be_life(hsize, vsize, birth_neighbors, life_neighbors):
+    for i in xrange(vsize):
+        for j in xrange(hsize):
+            life[j][i] = (Cell(j, i, birth_neighbors, life_neighbors))
+
+
+let_there_be_life(HSIZE, VSIZE, NEIGHBORS_FOR_BIRTH, NEIGHBORS_FOR_LIFE)
+print life
 
 while __name__ == '__main__':
-    pass
+    break
 
