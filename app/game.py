7f1ef96e2edbfa39c7d481b36d9d1608e954f0b7
Bradley Moore <BradleyMooreTX@gmail.com>
2014-10-06 10:12:14 -0600

    set to fullscreen at max resolution supported

--- app/game.py
+++ app/game.py
@@ -6,9 +6,12 @@ import pygame
 from life import Cell
 
 
-WIDTH = 800
-HEIGHT = 600
-SCREEN = pygame.display.set_mode([WIDTH,HEIGHT])
+pygame.display.init()
+modes = pygame.display.list_modes()
+
+WIDTH = modes[0][0]
+HEIGHT = modes[0][1]
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT),pygame.FULLSCREEN)
 
 TO_LIVE = [2, 3]
 TO_BE_BORN = [3]
