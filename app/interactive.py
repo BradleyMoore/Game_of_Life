2cf0f79aed15b1e03ce4275f6f1222af873507e1
BradleyMoore <BradleyMooreTX@gmail.com>
2014-10-09 01:05:28 -0600

    minor changes to the Button class

--- app/interactive.py
+++ app/interactive.py
@@ -1,20 +1,20 @@
 import pygame
 
 
-class button(object):
-    def __init__(self, screen, pos, width, height, color, text, text_color, text_size, text_pos):
+class Button(object):
+    def __init__(self, pos, width, height, color, text, text_color, text_size, text_pos):
         self.x = pos[0]
         self.y = pos[1]
         self.width = width
         self.height = height
         self.color = color
-        selt.text = text
+        self.text = text
         self.text_color = text_color
         self.text_size = text_size
         self.text_pos = text_pos
 
-    def draw(self):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, width, height))
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
         myfont = pygame.font.SysFont('monospace', self.text_size)
         label = myfont.render(self.text, 1, self.text_color)
         screen.blit(label, self.text_pos)
