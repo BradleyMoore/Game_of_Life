23d0410f7359928198bedda75c9e1228faed4555
Bradley Moore <BradleyMooreTX@gmail.com>
2014-10-09 13:49:20 -0600

    create grid on gamescene

--- app/scenes.py
+++ app/scenes.py
@@ -1,45 +1,53 @@
 import pygame
 
+from constants import HEIGHT, WIDTH, SCREEN
 from interactive import Button
 
 
 class Scene(object):
-    def __init__(self, height, width, background):
-        self.height = height
-        self.width = width
+    def __init__(self, background):
         self.background = background
-        self.items = []
+        self.rects = []
         self.texts = []
+        self.lines = []
         self.buttons = {}
 
-    def draw(self, screen):
-        screen.fill(self.background)
+    def draw(self, **kwargs):
+        SCREEN.fill(self.background)
 
-        for item in self.items:
-            pygame.draw.rect(screen, item['color'], (item['x'],item['y'],item['width'],item['height']), item['stroke'])
+        if 'life' in kwargs:
+            for cell in kwargs['life']:
+                cell.draw()
+
+        for rect in self.rects:
+            pygame.draw.rect(SCREEN, rect['color'], (rect['x'],rect['y'],rect['WIDTH'],rect['HEIGHT']), rect['stroke'])
+
+        for line in self.lines:
+            pygame.draw.line(SCREEN, line['color'], (line['startx'],line['starty']), (line['endx'],line['endy']), line['width'])
+
 
         for text in self.texts:
-            screen.blit(text[0], (text[1]))
+            SCREEN.blit(text[0], (text[1]))
 
         button_list = self.buttons.keys()
         for button in button_list:
-            self.buttons[button].draw_up(screen)
+            self.buttons[button].draw_up(SCREEN)
 
 
 class TitleScene(Scene):
-    def __init__(self, height, width, background):
-        Scene.__init__(self, height, width, background)
+    def __init__(self, background):
+        Scene.__init__(self, background)
 
         # start button
-        self.buttons['start'] = (Button('start', (self.width*.65,self.height*.67),
-                                self.width*.1, self.height*.1, (200,200,200),
+        self.buttons['start'] = (Button('start', (WIDTH*.65,HEIGHT*.67),
+                                WIDTH*.1, HEIGHT*.1, (200,200,200),
                                 'START', (100,100,100,), 40,
-                                (self.width*.655, self.height*.69)))
+                                (WIDTH*.655, HEIGHT*.69)))
 
         # black matte
-        self.items.append({'color': (0,0,0), 'x': 0, 'y': 0, 'width': self.width, 'height': self.height, 'stroke': self.height/9})
+        self.rects.append({'color': (0,0,0), 'x': 0, 'y': 0, 'WIDTH': WIDTH, 'HEIGHT': HEIGHT, 'stroke': HEIGHT/9})
         # red border
-        self.items.append({'color': (200,50,50), 'x': 0, 'y': 0, 'width': self.width, 'height': self.height, 'stroke': self.height/10})
+        self.rects.append({'color': (200,50,50), 'x': 0, 'y': 0, 'WIDTH': WIDTH, 'HEIGHT': HEIGHT, 'stroke': HEIGHT/10})
 
         # title
         myfont = pygame.font.SysFont('monospace', 200)
@@ -49,26 +57,16 @@ class TitleScene(Scene):
 
 
 class GameScene(Scene):
-    def __init__(self, height, width, background):
-        Scene.__init__(self, height, width, background)
+    def __init__(self, background):
+        Scene.__init__(self, background)
+
+        for i in xrange(WIDTH/150, WIDTH, WIDTH/150):
+            self.lines.append({'color': (100,100,100), 'startx': i, 'starty': 0, 'endx': i, 'endy': HEIGHT, 'width': 2})
+        for i in xrange(WIDTH/150, HEIGHT, WIDTH/150):
+            self.lines.append({'color': (100,100,100), 'startx': 0, 'starty': i, 'endx': WIDTH, 'endy': i, 'width': 2})
 
         # start button
-        self.buttons['start'] = (Button('start', (self.width*.65,self.height*.67),
-                                self.width*.1, self.height*.1, (200,200,200),
+        self.buttons['start'] = (Button('start', (WIDTH*.65,HEIGHT*.67),
+                                WIDTH*.1, HEIGHT*.1, (200,200,200),
                                 'START', (100,100,100,), 40,
-                                (self.width*.655, self.height*.69)))
-
-        # black matte
-        self.items.append({'color': (0,0,0), 'x': 0, 'y': 0, 'width': self.width, 'height': self.height, 'stroke': self.height/9})
-        # red border
-        self.items.append({'color': (200,50,50), 'x': 0, 'y': 0, 'width': self.width, 'height': self.height, 'stroke': self.height/10})
-
-        # title
-        myfont = pygame.font.SysFont('monospace', 200)
-        self.texts.append((myfont.render("Conway's", 1, (255,255,0)),(200,50)))
-        self.texts.append((myfont.render('Game of', 1, (255,255,0)),(250,250)))
-        self.texts.append((myfont.render('LIFE', 1, (255,255,0)),(300,450)))
-
-        def draw(self, screen, life):
-            for cell in life:
-                cell.draw(screen, height, width)
+                                (WIDTH*.655, HEIGHT*.69)))
